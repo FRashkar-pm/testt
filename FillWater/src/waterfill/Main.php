2ba3190923bc178fa40074f1c01df21ba06a538a
FRashkar-C <114340217+FRashkar-pm@users.noreply.github.com>
2023-02-16 12:26:13 +0700

    Update Main.php

--- FillWater/src/waterfill/Main.php
+++ FillWater/src/waterfill/Main.php
@@ -4,23 +4,15 @@ namespace waterfill;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-
 use pocketmine\event\Listener;
-
 use pocketmine\entity\Entity;
-
 use pocketmine\level\Level;
-
+use pocketmine\level\Position;
 use pocketmine\math\Vector3;
-
 use pocketmine\plugin\PluginBase;
-
 use pocketmine\Player;
-
 use pocketmine\Server;
-
 use pocketmine\block\Block;
-
 use pocketmine\item\Item;
 use pocketmine\item\ItemFactory;
 
@@ -29,6 +21,9 @@ class Main extends PluginBase implements Listener {
     public function onEnable(){
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->getLogger()->info("Plugin Enabled!");
+    }
+    public function onDisable(){
+        $this->getLogger()->info("Plugin Disabled!");
     }
 }
 
